a4bf3eb9771b001bb823b5156030909b336d59e2
Flipmemax <maxphoogland@gmail.com>
2021-05-07 12:26:01 +0200

    add "go to art feed" button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
     <div>
       <h1>HomePage</h1>
+      <Link to="/art">
+        <button>Go to Art Feed</button>
+      </Link>
     </div>
   );
 }
